6ea22933c5bee47cbe557215d04f764ad6b9538c
Azim Kubatov <46606593+Azim87@users.noreply.github.com>
2019-02-14 13:35:12 +0600

    added second activity and homework is DOne

--- app/src/main/java/com/kubatov/homework/MainActivity.java
+++ app/src/main/java/com/kubatov/homework/MainActivity.java
@@ -1,13 +1,37 @@
 package com.kubatov.homework;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button clickBtn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clickBtn = findViewById(R.id.firstButton);
+
+        clickBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
+        });
     }
+
+    private void goToSecondActivity(){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+
+    }
+
+
 }
