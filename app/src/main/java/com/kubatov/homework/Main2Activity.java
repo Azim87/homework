6804711e535f827f39a73032b83acce7f60d7261
Azim Kubatov <46606593+Azim87@users.noreply.github.com>
2019-02-15 21:16:54 +0600

    added 2nd 3rd activity and showed a message(1btn явный интент, 2btn не явный интент)

--- app/src/main/java/com/kubatov/homework/Main2Activity.java
+++ app/src/main/java/com/kubatov/homework/Main2Activity.java
@@ -1,13 +1,36 @@
 package com.kubatov.homework;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.security.PublicKey;
 
 public class Main2Activity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        textView = findViewById(R.id.textView_2);
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra("SOME_TEXT");
+        textView.setText(text);
+
+
+
+    }
+
+    public void onTextClick (View v) {
+        Intent intent = new Intent();
+        intent.putExtra("RETURN_TEXT", "Clicked");
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
--- app/src/main/java/com/kubatov/homework/MainActivity.java
+++ app/src/main/java/com/kubatov/homework/MainActivity.java
@@ -1,6 +1,7 @@
 package com.kubatov.homework;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button clickBtn;
-
+    Button clickBtn2;
 
 
     @Override
@@ -18,18 +19,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         clickBtn = findViewById(R.id.firstButton);
+        clickBtn2 = findViewById(R.id.secondButton);
 
         clickBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToSecondActivity();
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("SOME_TEXT", "This is from main activity");
+                startActivityForResult(intent, 1);
             }
         });
+
     }
 
-    private void goToSecondActivity(){
-        Intent intent = new Intent(this, Main2Activity.class);
-        startActivity(intent);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            String text = data.getStringExtra("RETURN_TEXT");
+            clickBtn.setText(text);
+
+        }
+    }
+
+    public void onHideClick(View v){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, "text to send");
+        intent.setType("text/plain");
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
 
     }
 
