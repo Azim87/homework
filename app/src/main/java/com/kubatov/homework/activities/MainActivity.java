679c78588469eec814329ded66ba06281d6ec6ba
Azim Kubatov <46606593+Azim87@users.noreply.github.com>
2019-02-21 00:45:48 +0600

    added endless scrolling (notifyDataSetChanged)

--- app/src/main/java/com/kubatov/homework/activities/MainActivity.java
+++ app/src/main/java/com/kubatov/homework/activities/MainActivity.java
@@ -1,11 +1,17 @@
 package com.kubatov.homework.activities;
 
 
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.AbsListView;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 
 import com.kubatov.homework.R;
@@ -22,26 +28,73 @@ public class MainActivity extends AppCompatActivity {
     TaskAdapter taskAdapter;
     RecyclerView.LayoutManager layoutManager;
 
-
-
-
-
     List<Task> tasks;
+    Boolean isScrolling = false;
+    int currentItems, totalItems, scrollOutItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         setTasks();
         initRecycler(tasks);
 
     }
 
-    void initRecycler (List<Task> taskList) {
+    void initRecycler (final List<Task> taskList) {
         layoutManager = new LinearLayoutManager(this);
         taskAdapter = new TaskAdapter(taskList);
         tasksList = findViewById(R.id.tasks_recycler);
         tasksList.setAdapter(taskAdapter);
         tasksList.setLayoutManager(layoutManager);
+
+            taskAdapter.notifyDataSetChanged();
+
+
+        tasksList.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                    isScrolling = true;
+                }
+
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                currentItems = layoutManager.getChildCount();
+                totalItems = layoutManager.getItemCount();
+
+                try {
+                    scrollOutItems = ((LinearLayoutManager) recyclerView.getLayoutManager()).findFirstVisibleItemPosition();
+                }catch (NullPointerException e){}
+
+                if (isScrolling && (currentItems + scrollOutItems == totalItems)){
+                    isScrolling = false;
+                    fetchData();
+                }
+
+
+
+            }
+        });
+    }
+
+    private void fetchData() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <5; i++){
+                    tasks.add(new Task("Task 1", R.drawable.pics,"Description 1"));
+                    taskAdapter.notifyDataSetChanged();
+                }
+            }
+        }, 500);
     }
 
     void setTasks(){
@@ -52,5 +105,7 @@ public class MainActivity extends AppCompatActivity {
         tasks.add(new Task("Task 4", R.drawable.pics,"Description 4"));
         tasks.add(new Task("Task 5", R.drawable.pics,"Description 5"));
     }
+
+
 
 }
